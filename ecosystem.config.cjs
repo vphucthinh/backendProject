fda96162a19a88755cc522de0e6bdc20145d90cc
DTNS2905 <151748870+DTNS2905@users.noreply.github.com>
2024-06-14 14:22:31 +0700

    config for ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,17 +2,18 @@ module.exports = {
     apps: [
         {
             name: "backend",
-            script: "./server.js", // Replace with your main entry file
-            watch: true,
-            instances: 1,
-            autorestart: true,
+            script: "/opt/render/project/server.js", // Replace with the full path to your main entry file
+            watch: true, // Enable if you want PM2 to watch for file changes and restart the app
+            instances: 1, // Number of instances to launch, typically 1 for most applications
+            autorestart: true, // Automatically restart the app if it crashes
+            max_memory_restart: "1G", // Restart if the memory usage exceeds this limit
             env: {
                 NODE_ENV: "development",
-                DB_URI: process.env.DB_URI,
+                DB_URI: process.env.DB_URI, // Use environment variable for database URI
             },
             env_production: {
                 NODE_ENV: "production",
-                DB_URI: process.env.DB_URI,
+                DB_URI: process.env.DB_URI, // Use environment variable for database URI
             },
         },
     ],
